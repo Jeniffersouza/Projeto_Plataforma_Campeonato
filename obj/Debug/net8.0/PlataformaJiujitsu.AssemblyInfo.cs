13d2d0ffbb8dcfb0a83aeca7f55bd589ff3eb5ff
Jeniffersouza <ajenifferbsouza@gmail.com>
2025-04-02 10:57:27 -0300

    acrescentado dockerfile

--- obj/Debug/net8.0/PlataformaJiujitsu.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlataformaJiujitsu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlataformaJiujitsu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ce105cfdb6559cf48932f9ee591b763baf47df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f63febeca7099a5cd5daab1e3577679bb444212")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlataformaJiujitsu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlataformaJiujitsu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
