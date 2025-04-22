67b4e2c85a0e337d9aca1d909c631a4caf9c6567
Jeniffer souza <ajenifferbsouza@gmail.com>
2025-04-22 18:08:00 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -87,10 +87,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-// Aplica automaticamente todas as migrations pendentes ao iniciar a aplicação
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
-}
 app.Run();
